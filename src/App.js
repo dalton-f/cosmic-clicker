7dc5fa9013d101511b18e29a228b0b23e86c6b70
Dalton <daltonfiggdavies@gmail.com>
2024-02-10 08:33:45 +0000

    Directory and export setup

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 import React from "react";
+import Clicker from "./components/Clicker";
+import Shop from "./components/Shop";
+
+export default function App() {
+  return (
+    <>
+      <Clicker />
+      <Shop />
+    </>
+  );
+}
