b6a14785784f8bbcd6bad518c77ba27aac12ecd4
Dalton <daltonfiggdavies@gmail.com>
2024-02-18 08:09:40 +0000

    Set up initial data skeleton

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from "react";
 import Clicker from "./components/Clicker";
 import Shop from "./components/Shop";
 
-const initialData = {};
+const initialData = {
+  clicker: {
+    currPixelCount: 0,
+    pixelsPerSecond: 0,
+  },
+  shop: {
+    item: {
+      owned: 0,
+      price: 0,
+    },
+  },
+};
 
 export default function App() {
   const [data, setData] = useState(initialData);
